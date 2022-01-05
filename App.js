ad5e59750a169aa8ebd7d42fdcb0acfe13fe93b1
firstChairCoder <md1qq7@gmail.com>
2022-01-05 04:54:02 +0100

    added basic pagination

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 /* eslint-disable react-native/no-inline-styles */
 
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableHighlight,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Card from "./src/components/Card/ItemCard";
@@ -17,13 +25,16 @@ const styles = StyleSheet.create({
 
 export default function App() {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(2);
+
+  let API = `https://rickandmortyapi.com/api/character/?page=${page}`;
 
   useEffect(() => {
     fetchChars();
-  }, []);
+  }, [API]);
 
   const fetchChars = () => {
-    fetch("https://rickandmortyapi.com/api/character/?page=2")
+    fetch(API)
       .then((response) => response.json())
       // .then((res) => setData([...data, ...res.results]))
       .then((res) => {
@@ -56,6 +67,67 @@ export default function App() {
           windowSize={3}
           maxToRenderPerBatch={5}
         />
+      </View>
+      <View
+        style={{
+          width: "100%",
+          height: "10%",
+          backgroundColor: "azure",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: 20,
+        }}
+      >
+        <TouchableHighlight
+          style={{
+            width: "30%",
+            height: 50,
+            backgroundColor: "red",
+            justifyContent: "center",
+            alignItems: "center",
+            // paddingHorizontal: 20,
+          }}
+          onPress={() => {
+            if (page === 1) {
+              return;
+            }
+            setPage(page - 1);
+          }}
+          activeOpacity={0.9}
+        >
+          <View
+            style={{
+              // width: "100%",
+              // height: 50,
+              // backgroundColor: "red",
+              paddingHorizontal: 20,
+            }}
+          >
+            <Text>Previous</Text>
+          </View>
+        </TouchableHighlight>
+
+        <Text style={{ textAlign: "center", fontSize: 24 }}>{page}</Text>
+
+        <TouchableHighlight
+          style={{
+            width: "30%",
+            height: 50,
+            backgroundColor: "green",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onPress={() => setPage(page + 1)}
+        >
+          <View
+            style={{
+              paddingHorizontal: 20,
+            }}
+          >
+            <Text>Next</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     </SafeAreaView>
   );
